0c01af8f59812378522ae2da751d767e8a5ceb07
satesorosoroyarimasuka-a11y <satesorosoroyarimasuka@gmail.com>
2026-07-12 14:02:41 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'bp-v19';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// ネットワーク優先：常に最新を取得し、オフライン時のみキャッシュにフォールバック
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
